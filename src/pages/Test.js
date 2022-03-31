e583981260c2a5dbadb67d2ee3eddc4eabf02b15
LeeEugene1 <uwm1004@gmail.com>
2022-04-01 02:13:08 +0900

    React LifeCycle

---
+++ src/pages/Test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LifeCycle from "../components/LifeCycle";
+
+function Test() {
+  return (
+    <div>
+      <h1>Test</h1>
+      <LifeCycle />
+    </div>
+  );
+}
+
+export default Test;
